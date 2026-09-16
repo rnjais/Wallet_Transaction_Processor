1979900f016eb7fe1d569b02fe18d000bdaecdc2
Aryan Jaiswal <your-github-aj80529754@gmail.com>
2026-09-16 20:38:04 +0530

    Initial project setup

---
+++ wallet-transaction-processor/src/main/java/com/assignment/wallet_transaction_processor/repository/WalletRepository.java
@@ -0,0 +1,15 @@
+package com.assignment.wallet_transaction_processor.repository;
+
+import com.assignment.wallet_transaction_processor.entity.Wallet;
+import jakarta.persistence.LockModeType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface WalletRepository extends JpaRepository<Wallet, UUID> {
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    Optional<Wallet> findByUserId(UUID userId);
+}
---
+++ wallet-transaction-processor/src/test/java/com/assignment/wallet_transaction_processor/WalletTransactionProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.wallet_transaction_processor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletTransactionProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
