b7b982f8920b4d660332e654fd5967f272e1b739
Aryan Jaiswal <your-github-aj80529754@gmail.com>
2026-09-16 20:43:52 +0530

    Transaction.java

---
+++ wallet-transaction-processor/src/main/java/com/assignment/wallet_transaction_processor/entity/Transaction.java
@@ -0,0 +1,93 @@
+package com.assignment.wallet_transaction_processor.entity;
+
+import com.assignment.wallet_transaction_processor.enums.TransactionStatus;
+import com.assignment.wallet_transaction_processor.enums.TransactionType;
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(
+        name = "transactions",
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "uk_transaction_id",
+                        columnNames = "transaction_id"
+                )
+        }
+)
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "transaction_id", nullable = false, unique = true)
+    private UUID transactionId;
+
+    @Column(nullable = false)
+    private UUID userId;
+
+    @Column(nullable = false, precision = 19, scale = 2)
+    private BigDecimal amount;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TransactionType type;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TransactionStatus status;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    public Transaction() {
+    }
+
+    public Transaction(
+            UUID transactionId,
+            UUID userId,
+            BigDecimal amount,
+            TransactionType type,
+            TransactionStatus status,
+            LocalDateTime createdAt
+    ) {
+        this.transactionId = transactionId;
+        this.userId = userId;
+        this.amount = amount;
+        this.type = type;
+        this.status = status;
+        this.createdAt = createdAt;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public UUID getTransactionId() {
+        return transactionId;
+    }
+
+    public UUID getUserId() {
+        return userId;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public TransactionType getType() {
+        return type;
+    }
+
+    public TransactionStatus getStatus() {
+        return status;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+}
