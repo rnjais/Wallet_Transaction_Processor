f68a78ef65dcb1d6ddb66e698be7ba0df2fa21b4
Aryan Jaiswal <your-github-aj80529754@gmail.com>
2026-09-16 21:02:16 +0530

    TransactionController.java

--- wallet-transaction-processor/src/main/java/com/assignment/wallet_transaction_processor/controller/TransactionController.java
+++ wallet-transaction-processor/src/main/java/com/assignment/wallet_transaction_processor/controller/TransactionController.java
@@ -1,4 +1,29 @@
 package com.assignment.wallet_transaction_processor.controller;
 
+import com.assignment.wallet_transaction_processor.dto.ProcessTransactionRequest;
+import com.assignment.wallet_transaction_processor.dto.TransactionResponse;
+import com.assignment.wallet_transaction_processor.service.TransactionService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/transactions")
 public class TransactionController {
+
+    private final TransactionService transactionService;
+
+    public TransactionController(TransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
+
+    @PostMapping("/process")
+    public ResponseEntity<TransactionResponse> processTransaction(
+            @Valid @RequestBody ProcessTransactionRequest request
+    ) {
+        TransactionResponse response =
+                transactionService.processTransaction(request);
+
+        return ResponseEntity.ok(response);
+    }
 }
